561a711961aa5caed890a0bac4f5588c661905e0
Minash23 <minas27188@gmail.com>
2024-09-05 10:39:32 -0500

    fixing average in main.java

--- main.java
+++ main.java
@@ -1,14 +1,17 @@
 public class Main {
-	public static void main(String[] args) {
-		int[] numbers = {1, 2, 3, 4, 5};
-		System.out.println("The average is: " +
-	average(numbers));
-}	
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println("The average is: " + average(numbers));
+    }
 
-// Non-working average function (needs a fix)
-public static double average(int[] nums) {
-	// Placeholder: This function is incomplete and
-	currently returns 0
-	return 0;
-	}
+    // Fixed average function
+    public static double average(int[] nums) {
+        int sum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+
+        return (double) sum / nums.length;
+    }
 }
